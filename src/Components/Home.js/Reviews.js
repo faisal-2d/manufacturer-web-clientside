aa527cf4cec40fe9c757edff3bb3534eaf789a52
faisal-2d <abdullah71faisal@gmail.com>
2022-05-26 14:43:32 +0300

    reviews reversed

--- src/Components/Home.js/Reviews.js
+++ src/Components/Home.js/Reviews.js
@@ -5,20 +5,25 @@ import ReviewCard from './ReviewCard';
 
 const Reviews = () => {
     const [reviews, setReviews] = useState([]);
+    
     useEffect(() => {
         const headers = { 
                 'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,                
             };
         axios.get('http://localhost:5000/reviews', {headers})
-    .then(data => setReviews(data.data));
+    .then(data => reversedReviews(data.data));
     } ,[]);
+
+    const reversedReviews = arr =>{
+        setReviews(arr.reverse());        
+    } 
 
     if(!reviews){
         return <Loading></Loading>
     }
     return (
         <div className='px-12'>
-            <p>Total Reviews: {reviews.length}</p>
+            {/* <p>Total Reviews: {reviews.length}</p> */}
             <h2 className='text-3xl font-bold text-center text-slate-600 my-20'>Customers' Review</h2>
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
